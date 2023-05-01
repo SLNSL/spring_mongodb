b02b45d8e9fd46753591b14390cfc2950806131f
SLNSL <nik231002@mail.ru>
2023-05-01 20:56:13 +0300

    Initial commit

---
+++ src/main/java/com/example/suka/SukaApplication.java
@@ -0,0 +1,13 @@
+package com.example.suka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SukaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SukaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/suka/SukaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.suka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SukaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
